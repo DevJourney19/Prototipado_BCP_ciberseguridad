844a43162fc956ec73dbc43be6d034c8a488a75e
DevJourney19 <F4r3ver@gmail.com>
2024-10-04 12:11:35 -0500

    feat: Se hicieron algunos cambios

---
+++ activacion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'fragmentos/head.php'>
+    <title>Activacion</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img src="img/loguito.png">
+            <i class="fa-solid fa-right-from-bracket fa-2xl"></i>
+        </nav>
+    </header>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN - ADMIN</title>
+    <link href="style_login.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <img src="img/loguito.png" alt="logo_bcp.jpg">
+    <div class="flex">
+        <img src="img/bcp_bycer.jpg" alt="bcp_cyber.jpg">
+        <div class="iniciar_sesion">
+            <h1>Ingresa tus datos de Admnistrador</h1>
+            <form action="#">
+                <table>
+                    <tr>
+                        <td><input type="text" placeholder="Email"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" placeholder="Usuario"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" placeholder="Contraseña"></td>
+                    </tr>
+                    <tr>
+                        <td><input class="button" type="submit" value="Ingresar"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
