42ea51feac8dd625508b5dbccf0084f024943d0f
Jose Balcazar <jbalcazar377@gmail.com>
2024-10-21 18:14:52 -0500

    tecladovirtual-cambiodevariablepinaclaveinternetporesolasclasesdeusuairoyloginestancambiadas

--- php/user_login.php
+++ php/user_login.php
@@ -1,45 +1,30 @@
 <?php
-
 include 'util/connection.php';
 
 $tarjeta = $_POST['tarjeta'];
 $dni = $_POST['dni'];
-$pin = $_POST['pin'];
+$clave_internet = $_POST['clave_internet']; // Cambié de $pin a $clave_internet
 
-$validar_login = "SELECT * FROM usuario WHERE numero_tarjeta = '$tarjeta' AND dni = '$dni' AND AES_DECRYPT(pin, '$pin') = '$pin'";
+// Corregir la consulta para desencriptar el pin almacenado y compararlo con el ingresado
+$validar_login = "SELECT * FROM usuario WHERE numero_tarjeta = '$tarjeta' AND dni = '$dni' AND AES_DECRYPT(clave_internet, 'D9u#F5h8*Z3kB9!nL7^mQ4') = '$clave_internet'";
 $verificar_entradas = "SELECT * FROM usuario WHERE numero_tarjeta = '$tarjeta' OR dni = '$dni'";
 
 try {
     conectar();
     $registro = consultar($validar_login);
-    // $entrada_no_deseada = consultar($verificar_entradas);
+    $entrada_no_deseada = consultar($verificar_entradas);
+    desconectar();
 
     if (count($registro) == 1) {
         session_start();
         $_SESSION['security'] = '12345';
         $_SESSION['id'] = $registro[0]['id_usuario'];
-        $verificar_seguridad = "SELECT * FROM seguridad WHERE id_usuario = " . $registro[0]['id_usuario'];
-        $seguridad = consultar($verificar_seguridad);
-        desconectar();
-        if (count($seguridad) == 1) {
-            $_SESSION['id_seguridad'] = $seguridad[0]['id_seguridad'];
-            echo "Ya existe";
-        }
-        unset($registro);
-        unset($seguridad);
         header("Location: ../principal.php");
-
-        //AQUI ->
-    }
-    // else if(count($entrada_no_deseada) == 1) {
-    //     session_start();
-    //     $_SESSION['id_no_permitido'] = $entrada_no_deseada[0]['id_usuario'];
-    //     header("Location: ../index.php?error=true");
-
-    //     //Como puedo hacer para que no me lleve a esa direccion solo que ejecute codigo?
-    //     // echo "<script>window.location.href = 'direccion_ip.php';</script>";
-    // }
-    else {
+    } else if(count($entrada_no_deseada) == 1) {
+        session_start();
+        $_SESSION['id_no_permitido'] = $entrada_no_deseada[0]['id_usuario'];
+        header("Location: ../index.php?error=true");
+    } else {
         session_destroy();
         header("Location: ../index.php?error=true");
     }
