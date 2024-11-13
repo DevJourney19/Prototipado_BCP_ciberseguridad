bc07ebee11a47f3049b5a21163533a241110cf39
Lena <102402197+Lenas25@users.noreply.github.com>
2024-11-13 14:56:09 -0500

    admi proceso

--- controller/ControllerAdmiLogin.php
+++ controller/ControllerAdmiLogin.php
@@ -1,24 +1,29 @@
 <?php
 include '../dao/DaoUsuario.php';
 
-$email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_NUMBER_INT);
-$password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_NUMBER_INT);
-$nombre = filter_input(INPUT_POST,'nombre',FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
+$nombre = filter_input(INPUT_POST,'nombre',FILTER_SANITIZE_SPECIAL_CHARS);
+$password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING);
 
+
+echo $email;
+echo $password;
+echo $nombre;
 
 $daoUsuario = new DaoUsuario();
 
-try {;
+try {
     $registro = $daoUsuario->verificarLoginAdmi($email, $nombre, $password);
+    echo $registro[0]['nombre'];
     if (count($registro) == 1) {
         session_start();
         $_SESSION['security'] = '12345';
         $_SESSION['id_usuario'] = $registro[0]['id_usuario'];
         header("Location: ../view/dashboard.php");
     } else {
-        session_start();
-        session_destroy();
-        header("Location: ../view/login_admin.php?error=true");
+        // session_start();
+        // session_destroy();
+        // header("Location: ../view/login_admin.php?error=true");
     }
 } catch (Exception $exc) {
     die($exc->getMessage());
