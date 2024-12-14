2f6216bf121220dae51b58383744f22aaa6dc9c0
DevJourney19 <F4r3ver@gmail.com>
2024-12-13 23:52:10 -0500

    feat: Cambiar configuración para subirlo a la nube.

--- config/db_config.php
+++ config/db_config.php
@@ -17,8 +17,11 @@ date_default_timezone_set("America/Lima");
 // $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 // $dotenv->load();
 
-# Propiedades de la base de datos usando variables de entorno
-define('HOST', 'localhost:3306');
-define('USER', 'root');
-define('PASS', '');
-define('DATABASE', 'seguridadbcp');
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+$db = mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_PORT");
+
+?>
